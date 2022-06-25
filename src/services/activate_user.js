f685f23b2fde3c7c254358693474a4ebe4aa1081
Quoc Nguyen <nguyenquoc18320@.gmail.com>
2022-06-25 16:37:12 +0700

    Add demo page, edit UI

---
+++ src/services/activate_user.js
@@ -0,0 +1,23 @@
+import Global from "../globals";
+
+ async function activateUser (accountid){
+    const requestOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + Global.access_token,
+      },
+    };
+  
+    //send to Api
+    const response = await fetch(Global.api_url + "activate-user?accountid=" + accountid, requestOptions);
+    const json = await response.json();
+    if (response.ok) {
+      // setData(json);
+      return json.data;
+    }else{
+      return null;
+    }
+  }
+
+export default activateUser;
